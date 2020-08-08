f918858674e9750de995085b5a701488dac38f87
Lucas Bigelow <lukejbigelow@gmail.com>
2020-08-08 12:09:15 -0600

    Using docs dir

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './index.js',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/docs',
     filename: 'index_bundle.js'
   },
   mode: 'development',
